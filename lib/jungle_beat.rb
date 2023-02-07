04038e489594f355c0b60b576742349b9037e4b3
Alejandro Lopez <alejandrolopez1592@gmail.com>
2023-02-07 11:40:40 -0700

    Feat: fix functionality of append method in jungle_beat

--- lib/jungle_beat.rb
+++ lib/jungle_beat.rb
@@ -9,22 +9,9 @@ class JungleBeat
 
   def append(sounds)
     sounds_array = sounds.split(' ')
-    active_node = @head
     sounds_array.each do |sound|
+      list.append(sound)
       require 'pry'; binding.pry
-      sound = Node.new(sound)
-      while list.head != nil
-        list = active_node.next_node
-      end
-      list = sound
-  end
-
-
-    # active_node = @head
-    # new_node = Node.new(beat)
-    # while active_node.next_node != nil  
-    #   active_node = active_node.next_node
-    # end
-    # active_node.next_node = new_node
+    end
   end
 end
