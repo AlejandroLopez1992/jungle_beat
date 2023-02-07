9e126c23a51a7f971f8812b83c017f4452618fd0
Alejandro Lopez <alejandrolopez1592@gmail.com>
2023-02-07 08:56:24 -0700

    Feat: Add non-working append method for jungle_beat

--- lib/jungle_beat.rb
+++ lib/jungle_beat.rb
@@ -6,4 +6,25 @@ class JungleBeat
   def initialize(list = nil)
     @list = LinkedList.new
   end
+
+  def append(sounds)
+    sounds_array = sounds.split(' ')
+    active_node = @head
+    sounds_array.each do |sound|
+      require 'pry'; binding.pry
+      sound = Node.new(sound)
+      while list.head != nil
+        list = active_node.next_node
+      end
+      list = sound
+  end
+
+
+    # active_node = @head
+    # new_node = Node.new(beat)
+    # while active_node.next_node != nil  
+    #   active_node = active_node.next_node
+    # end
+    # active_node.next_node = new_node
+  end
 end
